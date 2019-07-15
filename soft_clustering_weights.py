f2e044801f98106db3d5642b285bce17110a2da6
kpflugshaupt <kpflugshaupt@gmail.com>
2019-07-15 16:50:39 +0200

    Formatting

--- soft_clustering_weights.py
+++ soft_clustering_weights.py
@@ -19,12 +19,12 @@ def soft_clustering_weights(data, cluster_centres, **kwargs):
     # Get distances from the cluster centres for each data point and each cluster
     EuclidDist = np.zeros((Ndp, Nclusters))
     for i in range(Nclusters):
-        EuclidDist[:,i] = np.sum((data-np.matlib.repmat(cluster_centres[i], Ndp, 1))**2,axis=1)
+        EuclidDist[:,i] = np.sum((data - np.matlib.repmat(cluster_centres[i], Ndp, 1))**2, axis=1)
     
 
     
     # Denominator of the weight from wikipedia:
-    invWeight = EuclidDist**(2/(m-1))*np.matlib.repmat(np.sum((1./EuclidDist)**(2/(m-1)),axis=1).reshape(-1,1),1,Nclusters)
-    Weight = 1./invWeight
+    invWeight = EuclidDist**(2/(m-1)) * np.matlib.repmat(np.sum((1. / EuclidDist)**(2/(m-1)), axis=1).reshape(-1, 1), 1, Nclusters)
+    Weight = 1. / invWeight
     
     return Weight
